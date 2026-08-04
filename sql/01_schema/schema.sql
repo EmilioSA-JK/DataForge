e9506ab9b407863fc78290ca64fbb8a09abe6db4
AMD <224468537+EmilioSA-JK@users.noreply.github.com>
2026-08-03 21:15:18 -0600

    Aquí agrego el esquema inicial de la base de datos junto con su respectivo diagrama

---
+++ sql/01_schema/schema.sql
@@ -0,0 +1,74 @@
+# Se crea la base de datos y se pone en uso
+
+CREATE DATABASE PromesasIT;
+use PromesasIT;
+
+CREATE TABLE Personas(
+ced varchar(30) PRIMARY KEY,
+nombre varchar(40),
+ape01 varchar(40),
+ape02 varchar(40),
+relacion varchar(60),
+fecha_nacimiento date
+	);
+    
+    
+
+# Se empieza a crear el schema inicial 
+
+CREATE TABLE Rh(
+	carnet INT PRIMARY KEY AUTO_INCREMENT,
+    ced VARCHAR(30),
+    salario decimal(10,2),
+    fecha_ingreso date,
+    puesto varchar(60),
+	
+	FOREIGN KEY (ced) REFERENCES Personas(ced)
+	);
+    
+    
+CREATE TABLE Compras(
+	n_factura int PRIMARY KEY AUTO_INCREMENT,
+    proveedor varchar(30),
+    fecha date,
+    sub_total decimal(10,2),
+    IVA decimal(10,2),
+    total decimal(10,2),
+    
+    FOREIGN KEY(proveedor) REFERENCES Personas(ced)
+    
+    );
+    
+CREATE TABLE Inventario(
+	cod int PRIMARY KEY AUTO_INCREMENT,
+    descripcion VARCHAR(60),
+    costo decimal(10,2),
+    precio decimal(10,2),
+    carnet varchar(30),
+    saldo int,
+    
+    FOREIGN KEY(carnet) REFERENCES Personas(ced)
+    );
+    
+CREATE TABLE detalle_compras(
+	consecutivo INT PRIMARY KEY AUTO_INCREMENT,
+    n_factura INT,
+    linea INT,
+    cod INT,
+    cantidad INT,
+    
+    FOREIGN KEY (n_factura) REFERENCES Compras(n_factura),
+    FOREIGN KEY (cod) REFERENCES Inventario(cod)
+    
+	);
+    
+CREATE TABLE Avance(
+	carnet varchar(30) PRIMARY KEY,
+    saldo INT,
+    
+    FOREIGN KEY (carnet) REFERENCES Personas(ced)
+    );
+    
+    
+
+    
